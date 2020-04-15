e2eb72837192d20858627c15578e9a01c084aab2
IvonneOcampoR <56851079+IvonneOcampoR@users.noreply.github.com>
2020-04-15 04:40:46 -0700

    Add files via upload

---
+++ Discordia/src/app/tab1/tab1.page.ts
@@ -0,0 +1,41 @@
+import { Friend } from './../models/friend';
+import { ShareService } from './../services/share.service';
+import { Message } from '../models/message';
+import { DataService } from './../services/data.service';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  
+  myFriends: Friend[] = [];
+  allMessages : Message[] = [];
+  friendFilter = "Everyone";
+
+  constructor(private data : DataService, public shared: ShareService) {
+    //get data
+    data.getAllMessages().subscribe(list => {
+      console.log("messages", list);
+      this.allMessages = list.filter(m => m.to == "Everyone" || m.to == shared.userName || m.from == shared.userName);
+    });
+  
+   
+   
+    data.getAllFriends().subscribe(list => {
+    this.myFriends = list.filter(f => f.belongsTo == shared.userName);
+    });
+  
+  
+  }
+
+  getMessagesToDisplay(){
+    if(this.friendFilter == "Everyone")return this.allMessages;
+
+    return this.allMessages.filter(m => m.from == this.friendFilter || (m.from == this.shared.userName && m.to == this.friendFilter) );
+  }
+  
+}
